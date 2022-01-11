8cd6baddd188c7125adb6e4cb4c0ebe2ea180688
titacahyaa <95679672+titacahyaa@users.noreply.github.com>
2022-01-11 23:42:14 +0700

    Update Tulisan.java

--- src/tulisan/Tulisan.java
+++ src/tulisan/Tulisan.java
@@ -16,7 +16,7 @@ public class Tulisan {
      */
     public static void main(String[] args) {
         System.out.println("Tita adalah bestie vania, rr, bunga, dl");
-        System.out.println("tapi yang 1 sudah tdk menganggap aq bestie :p");
+        System.out.println("oemge :p");
     }
     
 }
