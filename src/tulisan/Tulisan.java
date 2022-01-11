e2558e99309f3ca0ca48a14a8873bf651e471481
titacahyaa <pratistha2602@gmail.com>
2022-01-11 23:39:18 +0700

    masukin project ke repo

---
+++ src/tulisan/Tulisan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tulisan;
+
+/**
+ *
+ * @author titacahyaa
+ */
+public class Tulisan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Tita adalah bestie vania, rr, bunga, dl");
+        System.out.println("tapi yang 1 sudah tdk menganggap aq bestie :p");
+    }
+    
+}
